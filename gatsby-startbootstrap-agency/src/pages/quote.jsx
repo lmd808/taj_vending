cfd6a73c1af7c9ffca75f3c81e2ea8f8ed3d8517
Laura DiTommaso <52080492+lmd808@users.noreply.github.com>
2021-04-29 19:44:02 -0400

    form begining phases

--- gatsby-startbootstrap-agency/src/pages/quote.jsx
+++ gatsby-startbootstrap-agency/src/pages/quote.jsx
@@ -4,26 +4,58 @@ import React from 'react'
 const quote = () => {
     return (
         <div>
-            <form name="contact" method="POST" data-netlify="true" contentType= 'application/x-www-form-urlencoded'>
-                <p>
-                    <label htmlFor='name'>Your Name: <input id='name' type="text" name="name" /></label>   
-                </p>
-                <p>
-                    <label htmlFor='email'>Your Email: <input id='email' type="email" name="email" /></label>
-                </p>
-                <p>
-                    <label htmlFor='role'>Your Role: <select name="role[]" multiple>
-                    <option value="leader">Leader</option>
-                    <option value="follower">Follower</option>
-                    </select></label>
-                </p>
-                <p>
-                    <label htmlFor='message'>Message: <textarea id='message' name="message"/></label>
-                </p>
-                <p>
-                    <button type="submit">Send</button>
-                </p>
-            </form>
+              <form
+                  method="post"
+                  name="contact-form"
+                  data-netlify="true"
+                  netlify-honeypot="bot-field"
+                  action="/thanks"
+                >
+                  <input
+                    type="hidden"
+                    name="contact-form"
+                    value="contact-form"
+                  />
+                  <div hidden>
+                    <label>
+                      Don’t fill this out: <input name="bot-field" />
+                    </label>
+                  </div>
+                  <div className="row uniform 50%">
+                    <div className="6u 12u$(xsmall)">
+                      <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder="Name"
+                      />
+                    </div>
+                    <div className="6u 12u$(xsmall)">
+                      <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="Email"
+                        required
+                      />
+                    </div>
+                    <div className="12u">
+                      <textarea
+                        name="message"
+                        id="message"
+                        placeholder="Message"
+                        rows="4"
+                      />
+                    </div>
+                  </div>
+                  <ul className="actions">
+                    <li className="form-submit">
+                      <button type="submit" className="button">
+                        Submit
+                      </button>
+                    </li>
+                  </ul>
+                </form>
             
         </div>
     )
