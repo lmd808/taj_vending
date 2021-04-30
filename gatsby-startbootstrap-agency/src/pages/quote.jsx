f329e11179575674c6747b1cdd15c83d29cdff2c
Laura DiTommaso <52080492+lmd808@users.noreply.github.com>
2021-04-29 21:10:19 -0400

    working form

--- gatsby-startbootstrap-agency/src/pages/quote.jsx
+++ gatsby-startbootstrap-agency/src/pages/quote.jsx
@@ -1,65 +1,83 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { navigate } from 'gatsby'
+  
+function encode(data) {
+    return Object.keys(data)
+      .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+      .join('&')
+  }
 
 
-const quote = () => {
+function Quote () {
+  
+    const [state, setState] = useState({})
+  
+    const handleChange = (e) => {
+      setState({ ...state, [e.target.name]: e.target.value })
+    }
+  
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      const form = e.target
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: encode({
+          'form-name': form.getAttribute('name'),
+          ...state,
+        }),
+      })
+        .then(() => navigate(form.getAttribute('action')))
+    }
+  
     return (
-        <div>
-              <form
-                  method="post"
-                  name="contact"
-                  data-netlify="true"
-                  netlify-honeypot="bot-field"
-                  action="/thanks"
-                >
-                  <input
-                    type="hidden"
-                    name="contact-form"
-                    value="contact-form"
-                  />
-                  <div hidden>
-                    <label>
-                      Don’t fill this out: <input name="bot-field" />
-                    </label>
-                  </div>
-                  <div className="row uniform 50%">
-                    <div className="6u 12u$(xsmall)">
-                      <input
-                        type="text"
-                        name="name"
-                        id="name"
-                        placeholder="Name"
-                      />
-                    </div>
-                    <div className="6u 12u$(xsmall)">
-                      <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        placeholder="Email"
-                        required
-                      />
-                    </div>
-                    <div className="12u">
-                      <textarea
-                        name="message"
-                        id="message"
-                        placeholder="Message"
-                        rows="4"
-                      />
-                    </div>
-                  </div>
-                  <ul className="actions">
-                    <li className="form-submit">
-                      <button type="submit" className="button">
-                        Submit
-                      </button>
-                    </li>
-                  </ul>
-                </form>
-            
-        </div>
+      <div>
+        <h1>Contact</h1>
+        <form
+          name="contact"
+          method="post"
+          action="/thanks"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={handleSubmit}
+        >
+          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+            <label>
+              Don’t fill this out: <input name="bot-field" onChange={handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your name:
+              <br />
+              <input type="text" name="name" onChange={handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your email:
+              <br />
+              <input type="email" name="email" onChange={handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message:
+              <br />
+              <textarea name="message" onChange={handleChange} />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </div>
     )
+
 }
 
 
-export default quote
+
+export default Quote
